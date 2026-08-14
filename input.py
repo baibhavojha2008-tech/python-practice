486a67c6199b2d4e027a01c970c85cf65e51b8e7
Baibhav_Ojha-DS <baibhav.ojha2008@gmail.com>
2026-08-14 09:03:41 +0545

    Add files via upload

---
+++ input.py
@@ -0,0 +1,7 @@
+a= int(input("Enter a number: ")) # input integer
+b= int(input("Enter another number: ")) # input integer
+
+print("The first number is: ", a) # print first number
+print("The second number is: ", b) # print second number        
+
+print("The sum of the two numbers is: ", a + b) # print sum of two numbers
---
+++ negative_slicing.py
@@ -0,0 +1,8 @@
+name="Baibhav"
+
+print(name[-1]) #output: v
+print(name[-2]) #output: a
+print(name[-3:-1]) #output: ha
+print(name[-7:-1]) #output: Baibha
+
+print(name[1:4:2]) #output: ab
