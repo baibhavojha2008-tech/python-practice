bf21f4969fd38f5913567dc50457dfbd630633a3
baibhavojha2008-tech <baibhav.ojha2008@gmail.com>
2026-08-19 22:54:07 +0545

    reorganise files into topic folders

---
+++ Ch_3_String/escape_seq.py
@@ -0,0 +1,6 @@
+a="This is a string with a newline character.\nAnd this is the next line."
+b= "This is a string with a tab character.\tAnd this is after the tab."
+c= "This is a string with a backslash character.\"Print with double quotes.\""
+print(a)
+print(b)
+print(c)
---
+++ Ch_3_String/problem_1.py
@@ -0,0 +1,5 @@
+letter= '''Dear <|Name|>,
+    I hope this letter finds you well.
+     <|Date|> '''
+
+print(letter.replace("<|Name|>","Baibhav").replace("<|Date|>","June 1, 2026"))
