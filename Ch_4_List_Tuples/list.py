92d867a45ffa37d18b4415027b83156c3aa15347
baibhavojha2008-tech <baibhav.ojha2008@gmail.com>
2026-08-19 23:58:38 +0545

    commint changes

---
+++ Ch_4_List_Tuples/list.py
@@ -0,0 +1,11 @@
+list=["Apple", "Banana", 5, 345.09, False, "Rohan"]
+
+print(list[0])  # Output: Apple
+print(list[1])  # Output: Banana
+print(list[2])  # Output: 5
+print(list[3])  # Output: 345.09
+print(list[4])  # Output: False
+print(list[5])  # Output: Rohan
+
+
+print(list[0:3])  # Output: ['Apple', 'Banana', 5]  
