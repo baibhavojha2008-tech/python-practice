f812d29f74b3cad9f15f36960a233420540bb463
Baibhav_Ojha-DS <baibhav.ojha2008@gmail.com>
2026-08-25 20:54:50 +0545

    Introduction on panads

---
+++ Ch_10_Pandas/01_Intro.py
@@ -0,0 +1,11 @@
+# Pandas = Panel data  || It works with series and data frame
+'''
+          SERIES VS DATA FRAME:
+1.SERIES:
+->  A Series is like a one dimensional labeled column.
+2.DATA_FRAME
+-> A DataFrame is like a two dimensional labeled column.
+'''
+ # Pandas can be said as ms excel version of python
+
+import pandas as pd
