65c8803c84ab4833d2d2777219237df9aa9df538
Baibhav_Ojha-DS <baibhav.ojha2008@gmail.com>
2026-08-31 00:21:50 +0545

    Add Matplotlib intro plotting examples
    Add two introductory Matplotlib scripts in Ch_11_Matplotlib/0_Intro: 0_intro.py demonstrates basic plt.plot() using Python lists; 01_using_numpy.py shows the same plot using NumPy arrays with a comment about array performance. Both scripts call plt.show() to display the plot.

---
+++ Ch_11_Matplotlib/0_Intro/01_using_numpy.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+x= np.array([2023,2024,2026,2028])
+y= np.array([15,20,25,10])
+
+plt.plot(x,y) # Calling Plot
+
+plt.show() # Showing plot graphically
+
+
+#Numpy array are faster than python list so we use it
---
+++ Ch_11_Matplotlib/0_Intro/0_intro.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+x= [2023,2024,2026,2028]
+y= [15,20,25,10]
+
+
+plt.plot(x,y) # Calling Plot
+
+plt.show() # Showing plot graphically
